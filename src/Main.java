e6b35f86a083914245aea370757a1b7b997af1fa
Helena Lykstoft <helena.botn.lykstoft@gmail.com>
2022-09-05 13:27:54 +0200

    adds notes and prints them

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Creates nodes with strings inside
+        Node node1 = new Node("1");
+        Node node2 = new Node("2");
+        Node node3 = new Node("3");
+        Node node0 = new Node("0");
 
+        // Makes the nodes connect to eachother
+        node0.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.prev = node2;
+        node2.prev = node1;
+        node1.prev = node0;
 
+        Node n = node0;
+
+        printFromHead(node0);
+
+    }
+
+    private static void printFromHead(Node n) {
+
+        while (n != null){
+            System.out.println(n.data);
+            n = n.next;
+        }
     }
 }
